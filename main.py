88834e02ebc6ff99bd98a740b9a2c4e73d1c1405
Lucas Honda <88567622+LucasHT22@users.noreply.github.com>
2025-08-10 13:25:00 -0400

    update

---
+++ main.py
@@ -0,0 +1,16 @@
+import badge
+from badge.input import Buttons
+
+FONT_SIZE = 25
+
+class App(badge.BaseApp):
+    def on_open(self):
+        badge.display.fill(1)
+        badge.display.nice_text("CHERRIES!!", 50, 100 - FONT_SIZE, FONT_SIZE)
+        badge.display.show()
+    
+    def loop(self):
+        if badge.input.get_button(Buttons.SW3):
+            badge.display.nice_text("Cherries cherries cherries!!", 50, 100 - FONT_SIZE, FONT_SIZE)
+            badge.display.fill(1)
+            badge.display.show()
